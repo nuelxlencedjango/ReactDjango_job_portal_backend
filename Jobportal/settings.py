51b6b441a4753146424aa63f0c493d33ebc4971c
nuelxlencedjango <nuelxlence@gmail.com>
2024-08-14 07:08:07 +0100

    db changes'

--- Jobportal/settings.py
+++ Jobportal/settings.py
@@ -157,11 +157,11 @@ DATABASES = {
     'default': dj_database_url.config(
         default={
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DATABASE_NAME'),
-            'USER': os.getenv('DATABASE_USER'),
-            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-            'HOST': os.getenv('DATABASE_HOST'),
-            'PORT': os.getenv('DATABASE_PORT'),
+            'NAME': os.getenv('DATABASE_NAME', ''),
+            'USER': os.getenv('DATABASE_USER', ''),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD', ''),
+            'HOST': os.getenv('DATABASE_HOST', ''),
+            'PORT': os.getenv('DATABASE_PORT', ''),
         }
     )
 }
