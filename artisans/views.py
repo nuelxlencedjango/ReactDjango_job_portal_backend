2d42b6f1ec70f49866caeee4d8a053f056a5a586
nuelxlencedjango <nuelxlence@gmail.com>
2024-08-17 08:38:06 +0100

    artisan search

--- artisans/views.py
+++ artisans/views.py
@@ -115,9 +115,10 @@ class ArtisanRegistrationView(APIView):
 class ArtisansByServiceView(APIView):
     permission_classes = [AllowAny]
     def get(self, request, service_title):
+        
         try:
             service = Service.objects.get(title=service_title)
-            artisans = Artisan.objects.filter(service=service)
+            artisans = Artisan.objects.filter(service=service_title)
             serializer = ArtisanSerializer(artisans, many=True)
             #print('product and services we offer',service)
             return Response(serializer.data, status=status.HTTP_200_OK)
