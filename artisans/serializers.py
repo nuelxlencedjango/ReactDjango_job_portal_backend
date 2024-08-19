cbf407ce817f146e5d441357984475813f66430c
nuelxlencedjango <nuelxlence@gmail.com>
2024-08-19 02:34:21 +0100

    artisan serializers

--- artisans/serializers.py
+++ artisans/serializers.py
@@ -32,7 +32,7 @@ class ServiceSerializer(serializers.ModelSerializer):
 
 
 
-class ArtisanSerializer(serializers.ModelSerializer):
+class hjkArtisanSerializer(serializers.ModelSerializer):
    # user = UserSerializer() 
     location = serializers.SerializerMethodField()
     service = serializers.SerializerMethodField()
@@ -59,6 +59,43 @@ class ArtisanSerializer(serializers.ModelSerializer):
             'id': obj.service.id,
             'title': obj.service.title
         } if obj.service else None
+
+
+
+
+
+
+
+
+
+class ArtisanSerializer(serializers.ModelSerializer):
+    profile_img = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Artisan
+        fields = ['user', 'nin', 'location', 'experience', 'address', 'phone', 'service', 'profile_img', 'date_joined']
+        read_only_fields = ['date_joined']
+
+    def __init__(self, *args, **kwargs):
+        super(ArtisanSerializer, self).__init__(*args, **kwargs)
+        request_method = self.context.get('request').method if self.context.get('request') else None
+
+        if request_method in ['GET']:
+            # Using nested serializers for fetching (GET request)
+            self.fields['location'] = AreaSerializer()
+            self.fields['service'] = ServiceSerializer()
+            user = UserSerializer()
+        else:
+            # Using primary key related fields for creating/updating (POST, PUT, PATCH requests)
+            self.fields['location'] = serializers.PrimaryKeyRelatedField(queryset=Area.objects.all())
+            self.fields['service'] = serializers.PrimaryKeyRelatedField(queryset=Service.objects.all())
+            #self.fields['user'] = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    def get_profile_img(self, obj):
+        if obj.profile_img:
+            return obj.profile_img.url
+        return None
+
 
 
 
