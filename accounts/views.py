a96eff66b71f94e28b42eb4487402c309a64e2f5
nuelxlencedjango <nuelxlence@gmail.com>
2024-08-30 07:33:57 +0100

    user authorization

--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,9 @@ from rest_framework import generics, status,serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 
-#from django.contrib.auth.models import User
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
+
 from rest_framework.views import APIView
-
-
 
 from accounts.serializers import *
 from artisans.models import *
@@ -77,7 +73,7 @@ class mkLoginView(APIView):
 
 
 
-class LoginView(TokenObtainPairView):
+class bbLoginView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         tokens = response.data
@@ -98,6 +94,71 @@ class LoginView(TokenObtainPairView):
             domain=settings.SIMPLE_JWT['AUTH_COOKIE_DOMAIN'],
             path=settings.SIMPLE_JWT['REFRESH_COOKIE_PATH'],
         )
+        return response
+
+
+
+
+
+
+# views.py
+
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+
+User = get_user_model()
+
+def set_cookie(response, token, cookie_name):
+    response.set_cookie(
+        cookie_name,
+        token,
+        httponly=True,
+        secure=True,
+        samesite='Lax',
+        path='/',
+    )
+
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+
+        response = Response({
+            'refresh': refresh_token,
+            'access': access_token,
+        })
+        set_cookie(response, access_token, 'access_token')
+        set_cookie(response, refresh_token, 'refresh_token')
+
+        return response
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        tokens = response.data
+        set_cookie(response, tokens['access'], 'access_token')
+        set_cookie(response, tokens['refresh'], 'refresh_token')
+        return response
+
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        tokens = response.data
+        set_cookie(response, tokens['access'], 'access_token')
         return response
 
 
