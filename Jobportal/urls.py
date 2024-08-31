4ff963ed2164748aa62b353726f647b8dee0824e
nuelxlencedjango <nuelxlence@gmail.com>
2024-08-31 03:47:30 +0100

    user authorization

--- Jobportal/urls.py
+++ Jobportal/urls.py
@@ -21,7 +21,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 #from  accounts.views import *
-from accounts.views import LoginView, CustomTokenObtainPairView, CustomTokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+from accounts.views import LoginView
+#, CustomTokenObtainPairView, CustomTokenRefreshView
 
 
 urlpatterns = [
@@ -29,12 +32,12 @@ urlpatterns = [
      path('accounts/login/', LoginView.as_view(), name='login'),
 
    #tokenobtainedpairview
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    #path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    #path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
    
-   # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-   # path('api-auth/', include('rest_framework.urls')),  
+   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include('rest_framework.urls')),  
 
     path('api/', include('api.urls')),
     path('artisans/', include('artisans.urls')),
@@ -47,3 +50,11 @@ urlpatterns = [
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
+#from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+#urlpatterns = [
+ #   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  #  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+   # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+#]
