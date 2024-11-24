25e0c043fa713a73f4982ee0affa323df1ac0df8
nuelxlencedjango <nuelxlence@gmail.com>
2024-11-24 11:06:49 +0000

    already in the cart

--- employers/urls.py
+++ employers/urls.py
@@ -4,7 +4,6 @@ from .views import *
 from . import views
 
 
-
 urlpatterns = [
    path('add_employer/', EmployerCreateView.as_view(), name='add_employer'),
     path('order-request/', OrderRequestCreateView.as_view(), name='order-request'),
