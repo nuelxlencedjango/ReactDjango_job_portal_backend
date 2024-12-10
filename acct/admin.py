4426675cdc79749b2d1450a5a93406275704d3c3
nuelxlencedjango <nuelxlence@gmail.com>
2024-12-10 00:46:33 +0000

    admin

--- acct/admin.py
+++ acct/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import CustomUser, ArtisanProfile, EmployerProfile, ManagerProfile
+#from accounts.models import *
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    
+    list_display = ('username','first_name','last_name','email','artisan','employer','manager','admin')
+
+
+
+admin.site.register(CustomUser)
+
+
+admin.site.register(ArtisanProfile)
+admin.site.register(EmployerProfile)
+admin.site.register(ManagerProfile)
