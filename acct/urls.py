4fb684bda06e40d67d890f9073b3d80cabcec6e2
nuelxlencedjango <nuelxlence@gmail.com>
2024-12-10 18:01:57 +0000

    api serializer

--- acct/urls.py
+++ acct/urls.py
@@ -4,9 +4,11 @@ from . import views
 from .views import *
 
 urlpatterns = [
-    path('register/artisan/', views.ArtisanRegistrationView.as_view(), name='artisan-register'),
-    path('register/employer/', views.EmployerRegistrationView.as_view(), name='employer-register'),
-    path('register/manager/', views.ManagerRegistrationView.as_view(), name='manager-register'),
+    path('artisan-register/', views.ArtisanRegistrationView.as_view(), name='artisan-register'),
+    path('employer-register/', views.EmployerRegistrationView.as_view(), name='employer-register'),
+   # path('register/manager/', views.ManagerRegistrationView.as_view(), name='manager-register'),
+    path('add_artisan/', ArtisanRegistrationView.as_view(), name='add_artisan'),
+   
 ]
 
 
