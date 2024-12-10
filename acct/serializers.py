8c91fb4493a2275636f8f204b60b31f5b994c6b9
nuelxlencedjango <nuelxlence@gmail.com>
2024-12-10 02:23:48 +0000

    admin serializer

--- acct/serializers.py
+++ acct/serializers.py
@@ -5,7 +5,7 @@ from .models import ArtisanProfile, EmployerProfile, ManagerProfile
 
 User = get_user_model()
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class klkkCustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     confirm_password = serializers.CharField(write_only=True)
 
@@ -17,3 +17,43 @@ class CustomUserSerializer(serializers.ModelSerializer):
         if data['password'] != data['confirm_password']:
             raise serializers.ValidationError("Passwords do not match")
         return data
+
+
+
+
+
+
+from rest_framework import serializers
+from .models import CustomUser, ArtisanProfile, EmployerProfile
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'confirm_password']
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class ArtisanProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArtisanProfile
+        fields = ['experience', 'job_type', 'industry', 'pay', 'profile_image', 'fingerprint_image', 'nin']
+
+class EmployerProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmployerProfile
+        fields = ['company_name', 'company_address']
