c6d8f2df1874139e196b02ce34f62bc1df6df895
nuelxlencedjango <nuelxlence@gmail.com>
2024-12-10 02:24:24 +0000

    admin serializer

--- acct/views.py
+++ acct/views.py
@@ -78,3 +78,45 @@ class ManagerRegistrationView(APIView):
         )
         
         return Response({"message": "Manager registered successfully!"}, status=status.HTTP_201_CREATED)
+
+
+
+
+
+
+
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import CustomUser, ArtisanProfile, EmployerProfile
+from .serializers import CustomUserSerializer, ArtisanProfileSerializer, EmployerProfileSerializer
+
+class ArtisanRegistrationView(APIView):
+    def post(self, request):
+        user_serializer = CustomUserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            artisan_profile_data = request.data.copy()  # Copy request data
+            artisan_profile_data['user'] = user.id
+            artisan_serializer = ArtisanProfileSerializer(data=artisan_profile_data)
+            if artisan_serializer.is_valid():
+                artisan_serializer.save()
+                return Response({'detail': 'Registration successful!'}, status=status.HTTP_201_CREATED)
+            return Response(artisan_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EmployerRegistrationView(APIView):
+    def post(self, request):
+        user_serializer = CustomUserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            employer_profile_data = request.data.copy()
+            employer_profile_data['user'] = user.id
+            employer_serializer = EmployerProfileSerializer(data=employer_profile_data)
+            if employer_serializer.is_valid():
+                employer_serializer.save()
+                return Response({'detail': 'Registration successful!'}, status=status.HTTP_201_CREATED)
+            return Response(employer_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
