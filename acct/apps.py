3bf1a3783b1b1baf108d8d8ac69b7a4ec30af103
nuelxlencedjango <nuelxlence@gmail.com>
2024-12-11 03:35:45 +0000

    new app

--- acct/apps.py
+++ acct/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class AcctConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'acct'
+
+    def ready(self):
+        import acct.signal # Import the signals when the app is ready
+
+
+
