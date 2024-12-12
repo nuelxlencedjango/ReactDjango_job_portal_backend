84ca85c489cdd45a9f31dfeff692e8ec0fd03532
nuelxlencedjango <nuelxlence@gmail.com>
2024-12-12 02:36:42 +0000

    new endpoint

--- acct/signal.py
+++ acct/signal.py
@@ -1,10 +1,8 @@
 
-# signal.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import CustomUser, ArtisanProfile, EmployerProfile, ManagerProfile
 
-# Signal to Automatically Create Profiles
 @receiver(post_save, sender=CustomUser)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
@@ -15,7 +13,6 @@ def create_user_profile(sender, instance, created, **kwargs):
         elif instance.user_type == 'manager':
             ManagerProfile.objects.create(user=instance)
 
-# Signal to Automatically Save Profiles
 @receiver(post_save, sender=CustomUser)
 def save_user_profile(sender, instance, **kwargs):
     if hasattr(instance, 'artisanprofile'):
@@ -24,7 +21,3 @@ def save_user_profile(sender, instance, **kwargs):
         instance.employerprofile.save()
     elif hasattr(instance, 'managerprofile'):
         instance.managerprofile.save()
-
-
-
-
