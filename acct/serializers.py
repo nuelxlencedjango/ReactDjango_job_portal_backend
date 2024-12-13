651dfc2b90cac81e11d0ea01544f2a437060ef23
nuelxlencedjango <nuelxlence@gmail.com>
2024-12-13 17:03:14 +0000

    artisan class

--- acct/serializers.py
+++ acct/serializers.py
@@ -15,14 +15,14 @@ from .models import CustomUser, ArtisanProfile, EmployerProfile
 
 class rtCustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    confirm_password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True) 
 
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password','first_name','last_name',]
 
     def validate(self, data):
-        if data['password'] != data['confirm_password']:
+        if data['password'] != data['confirm_password']: 
             raise serializers.ValidationError("Passwords do not match.")
         return data
 
@@ -56,7 +56,7 @@ class ppEmployerProfileSerializer(serializers.ModelSerializer):
 
 class CpoustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    #confirm_password = serializers.CharField(write_only=True)
+    #confirm_password = serializers.CharField(write_only=True) 
 
     class Meta:
         model = CustomUser
@@ -81,9 +81,9 @@ class CpoustomUserSerializer(serializers.ModelSerializer):
 
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class CaqustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-   # confirm_password = serializers.CharField(write_only=True)
+   # confirm_password = serializers.CharField(write_only=True) 
 
     class Meta:
         model = CustomUser
@@ -92,7 +92,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
    # def validate(self, data):
         # Check if passwords match
-    #    if data['password'] != data['confirm_password']:
+    #    if data['password'] != data['confirm_password']: 
      #       raise serializers.ValidationError("Passwords do not match.")
       #  return data
 
@@ -115,6 +115,54 @@ class CustomUserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+
+
+
+from rest_framework import serializers
+from .models import CustomUser
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'confirm_password', 'first_name', 'last_name', 'user_type']
+        extra_kwargs = {'password': {'write_only': True}, 'confirm_password': {'write_only': True}}
+
+    def validate(self, data):
+        # Check if passwords match
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def validate_email(self, value):
+        # Ensure email is unique
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email is already taken.")
+        return value
+
+    def create(self, validated_data):
+        # Remove confirm_password from the validated data since it's not needed for user creation
+        validated_data.pop('confirm_password', None)
+
+        # Create the user with the validated data
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            user_type=validated_data.get('user_type', 'default_value')  # Default value if user_type is not provided
+        )
+
+        # Set and hash the password before saving
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+
 
 
 
