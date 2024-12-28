e5d71863583a1f74581295878f6ac78f88222f1c
nuelxlencedjango <nuelxlence@gmail.com>
2024-12-28 12:15:59 +0000

    employer cartitems

--- acct/models.py
+++ acct/models.py
@@ -71,6 +71,10 @@ class ArtisanProfile(BaseProfile):
     pay = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
 
+    def __str__(self):
+        return f"{self.service.title}"
+
+
 # Employer Profile
 class EmployerProfile(BaseProfile):
     
