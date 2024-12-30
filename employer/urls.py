eca16b8913b0ecaf1d79291179eb917a4e2b99f7
nuelxlencedjango <nuelxlence@gmail.com>
2024-12-30 01:41:46 +0000

    employer's details

--- employer/urls.py
+++ employer/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
   path('check-artisan/<str:artisan_email>/', CheckArtisanInCartView.as_view(), name='check-artisan'),
    path('add_to_cart/', AddToCartView.as_view(), name='add_to_cart'),
    path('cart-items/', CartItemsView.as_view(), name='cart-items'),
-   path('cart/<int:pk>/', CartItemsView.as_view(), name='cart-item-detail'),
+   #path('cart/<int:pk>/', CartItemsView.as_view(), name='cart-item-detail'),
+  # path('employer/cart-items/', get_cart_items, name='get_cart_items'),
    path('checkout/', CheckoutView.as_view(), name='checkout'),
    path('job-details/', JobDetailsView.as_view(), name='job-details'),
    
