aa36c144d51f0b3bff0f492721bd2c29c4c634ff
nuelxlencedjango <nuelxlence@gmail.com>
2025-01-31 03:15:25 +0000

    artisan search

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,10 +1,11 @@
 # urls.py
 from django.urls import path, include
 
-from .views import ArtisanProfileViewSet ,ArtisanSearchView
+from .views import ArtisanSearchView2 ,ArtisanSearchView
 
 
 urlpatterns = [
     path('artisans/search/', ArtisanSearchView.as_view(), name='artisan_search'),
-    path('api/artisans/search/', ArtisanProfileViewSet.as_view(), name='artisan_search'),
+    path('artisans/search/', ArtisanSearchView2.as_view(), name='artisan_search2'),
+    
 ]
