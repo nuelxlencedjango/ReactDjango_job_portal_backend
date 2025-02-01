e3981ad03d14d45ffe69af9b0080e18aaa1330e7
nuelxlencedjango <nuelxlence@gmail.com>
2025-02-01 15:01:03 +0000

    manager creation

--- acct/admin.py
+++ acct/admin.py
@@ -1,18 +1,32 @@
 from django.contrib import admin
 from .models import CustomUser, ArtisanProfile, EmployerProfile, ManagerProfile,BaseProfile
-#from accounts.models import *
-# Register your models here.
-
-
-class UserAdmin(admin.ModelAdmin):
-    
-    list_display = ('username','first_name','last_name','email','artisan','employer','manager','admin','date_joined')
+from django.contrib.auth.admin import UserAdmin
 
 
 
-admin.site.register(CustomUser)
-#admin.site.register(BaseProfile)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'user_type', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'user_type')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'user_type'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
 
 admin.site.register(ArtisanProfile)
 admin.site.register(EmployerProfile)
 admin.site.register(ManagerProfile)
+
+
