4b81da1f617e0d94b3751c965b0051a888f7c1a6
nuelxlencedjango <nuelxlence@gmail.com>
2025-02-12 23:39:54 +0000

    migrations

--- employer/models.py
+++ employer/models.py
@@ -121,7 +121,7 @@ class PaymentInformation(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="payments")
     tx_ref = models.CharField(max_length=100, unique=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    transaction_id = models.CharField(max_length=100, unique=True, default="0")
+    transaction_id = models.CharField(max_length=100, unique=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
