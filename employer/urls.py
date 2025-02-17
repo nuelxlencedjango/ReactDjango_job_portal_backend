32769f9585c07d823b8474fe6511ae06cf2432e2
nuelxlencedjango <nuelxlence@gmail.com>
2025-02-17 16:24:39 +0000

    initiate payment

--- employer/urls.py
+++ employer/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
    path('job-details/', JobDetailsView.as_view(), name='job-details'),
    
    path('payment-details/', InitiatePayment.as_view(), name='payment-details'),
-   path('payment_confirmation/', PaymentCallback.as_view(), name='payment_confirmation'),
+   path('payment_confirmation/', ConfirmPayment.as_view(), name='payment_confirmation'),
   # path("payment-details/", PaymentDetailsView.as_view(), name="payment-details"),
   #path('payment_confirmation/', views.handle_flutterwave_payment, name='payment_confirmation'),
    #path('employer-details/', EmployersDetailsView.as_view(), name='employer-details'),
