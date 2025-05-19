c1d8403ee30b2d7a9ee72438bcd4d7274123f347
nuelxlencedjango <nuelxlence@gmail.com>
2025-05-19 17:58:04 +0100

    list of registered artisans

--- marketers/urls.py
+++ marketers/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from .views import ArtisanRegistrationView
+from .views import ArtisanRegistrationView,ListArtisansView
 
 urlpatterns = [
     path('artisan-register/', ArtisanRegistrationView.as_view(), name='artisan_register'),
-    
+    path('list-registered-artisans/', ListArtisansView.as_view(), name='list-registered-artisans'),
+   
 ]
-
 
