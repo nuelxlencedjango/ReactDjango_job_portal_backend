e9112cae4467dcbb47cb31924bdbd3d1d2b83eb9
nuelxlencedjango <nuelxlence@gmail.com>
2025-05-19 15:10:23 +0100

    marketer image

--- acct/models.py
+++ acct/models.py
@@ -149,7 +149,7 @@ class EmployerProfile(BaseProfile):
 # Manager Profile
 class ManagerProfile(BaseProfile):
     department = models.CharField(max_length=100, null=True, blank=True)
-    
+    profile_image = CloudinaryField(null=True, blank=True)
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
 
