54d13fc0708b42920dfe71db0eef6baf6d94cc17
nuelxlencedjango <nuelxlence@gmail.com>
2025-09-04 13:19:24 +0100

    setting email config

--- Jobportal/settings.py
+++ Jobportal/settings.py
@@ -290,13 +290,13 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
    #EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST='smtp.office365.com'
-EMAIL_HOST_USER='contactus@i-wan-wok.com'
+EMAIL_HOST=os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'contactus@i-wan-wok.com'
+EMAIL_PORT =os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_BACKEND =os.getenv('EMAIL_BACKEND')
+DEFAULT_FROM_EMAIL =os.getenv('DEFAULT_FROM_EMAIL')
 
 # DJANGO_REST_PASSWORDRESET_TOKEN_CONFIG = {
 #     'SERIALIZER': 'acct.serializers.CustomPasswordResetSerializer'
