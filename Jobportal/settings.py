16a0551c47db45d03c39bdcfe9493f6e5b3ab81e
nuelxlencedjango <nuelxlence@gmail.com>
2025-09-08 01:33:48 +0100

    email config

--- Jobportal/settings.py
+++ Jobportal/settings.py
@@ -296,6 +296,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.office365.com') 
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True if os.getenv('EMAIL_USE_TLS', 'True') == 'True' else False  
+EMAIL_USE_SSL = False 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')              
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')       
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER) 
@@ -303,6 +304,8 @@ DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 # DJANGO_REST_PASSWORDRESET_TOKEN_CONFIG = {
 #     'SERIALIZER': 'acct.serializers.CustomPasswordResetSerializer'
 # }
+
+
 
 # Define CSRF trusted origins
 csrf_trusted_origins_env = os.getenv('CSRF_TRUSTED_ORIGINS', '')
