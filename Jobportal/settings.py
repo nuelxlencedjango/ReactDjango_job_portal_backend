fa0188bbcb77fc8fa54d345ec3de5d026dbde09f
nuelxlencedjango <nuelxlence@gmail.com>
2025-09-26 00:10:57 +0100

    email ssl

--- Jobportal/settings.py
+++ Jobportal/settings.py
@@ -291,19 +291,21 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 
+# DJANGO_REST_PASSWORDRESET_TOKEN_CONFIG = {
+#     'SERIALIZER': 'acct.serializers.CustomPasswordResetSerializer'
+# }
+
+
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.office365.com') 
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = True if os.getenv('EMAIL_USE_TLS', 'True') == 'True' else False  
+
+EMAIL_USE_TLS = True 
 EMAIL_USE_SSL = False 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')              
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')       
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER) 
-
-# DJANGO_REST_PASSWORDRESET_TOKEN_CONFIG = {
-#     'SERIALIZER': 'acct.serializers.CustomPasswordResetSerializer'
-# }
 
 
 
